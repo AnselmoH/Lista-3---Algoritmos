b6c47c32b1750757b2605dc88e39497b6b43d647
Héricles Anselmo <39626779+AnselmoH@users.noreply.github.com>
2019-05-23 00:19:01 -0300

    Create questao7.c

---
+++ questao7.c
@@ -0,0 +1,14 @@
+//questao 07 
+
+#include <stdio.h>
+
+int main(){
+    int x, z;
+    
+    for(x = 0; x <= 10; x++){
+        
+        z = 4*x;
+        printf (" 4 x %d = %d \n", x, z);      
+    }   
+    return 0;
+}
