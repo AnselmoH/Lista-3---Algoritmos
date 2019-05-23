8e55658a6be8a361bffdd95bf6478b1583ec9ccb
Héricles Anselmo <39626779+AnselmoH@users.noreply.github.com>
2019-05-23 00:18:13 -0300

    Create questao6.c

---
+++ questao6.c
@@ -0,0 +1,19 @@
+//questao 06
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+
+int x, L;
+
+    printf ("Informe o valor de L: \n");
+    scanf ("%d", &L);
+
+    x = sqrt(L)+1;
+
+    printf ("O menor inteiro positivo X eh %d \n", x);
+
+    return 0;
+    }
